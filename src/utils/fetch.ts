5bced41622b42892f771a6991f6ba7011b4347cd
Von Harrison Tadaiesky <vonharrison26@gmail.com>
2024-08-26 16:07:06 -0300

    feat: finishd project

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,6 +1,5 @@
 import api from "../services/config/api.ts";
 
-
 export const fetchData = async <T>(url: string, search?: string): Promise<T> => {
     const isValidation = search?.length !== 0 ?  `repo:voncardoso/blog-gitHub ${search}` : "repo:voncardoso/blog-gitHub";
     try {
